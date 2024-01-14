6cc6c945aa4ed72ac5285df74bddd24680545c52
Nguyen Truong <nguyen.truong@c0x12c.com>
2024-01-14 11:24:41 +0700

    Refactor code Add test CluserService Update Readme

--- README.md
+++ README.md
@@ -3,37 +3,63 @@ AI server for AI tasks: Validation problem submission, program clustering
 
 # Basic usage:
 
-1. Install necessary packages:
+1. Activate the venv
+```bash
+source venv/bin/activate
+```
+2. Install necessary packages:
 ```shell
 pip install -r requirements.txt
 ```
-2. Run the server:
+3. Run the server:
 ```shell
 python main.py
 ```
-3. API Requests: 
+# API Usage
+API Requests: 
 - **POST URL**: http://127.0.0.1:5000/api/v1/validate-description
     - Request body: JSON format
     ```json
-        {
-            "description": "problem description" 
-        }
+    {
+        "description": "problem description" 
+    }
     ```
     - Response:
     ```json
-        {
-            "isValid": Boolean 
-        }
+    {
+        "isValid": Boolean 
+    }
     ```
-
+- **POST URL**: http://127.0.0.1:5000/api/v1/cluster-programs
+    - Request body: JSON format
+    ```json
+    {
+        "submissions": [
+            {1: "Submission 1"},
+            {2: "Submission 2"},
+            {3: "Submission 3"},
+            {4: "Submission 4"}, ...
+        ]
+    }
+    ```
+    - Response:
+    ```json
+    {
+        "clusters": [
+            ["1", "2", "3", ...],
+            ["5", "6", "7", ...], ...
+        ],
+        "no_cluster": Integer
+    }
+    ```
 # Change processing model
 
 ## Validation endpoint
 
-Change these path (in `main.py`) to your prefer encoder and classifier
+Change these paths (in `/services/ValidationService.py`) to your prefer encoder and classifier
 ```python
-encoder = pickle.load(open('./pre-trained-encoders/tf_vectorize', 'rb'))
-validate_model = pickle.load(open('./pre-trained-models/svm_tf', 'rb'))
+self.encoder = pickle.load(open('./pre-trained-encoders/tf_vectorize', 'rb'))
+self.validate_model = pickle.load(open('./pre-trained-models/svm_tf', 'rb'))
 ```
 
 This repo currently supports 2 type:
@@ -41,4 +67,5 @@ This repo currently supports 2 type:
 - `tfidf_vectorize` encoder along with `svm_tfidf` or `naive_bayes_tfidf` classifier.
 
 ## Program clustering endpoint (`In progress`)
+Currently, the cluster-program endpoint is for testing purpose only.
 
--- main.py
+++ main.py
@@ -1,35 +1,81 @@
-import pickle
-from flask import Flask, jsonify
+from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+from services.ValidationService import ValidationService
+from services.ClusterService import ClusterService
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument("description")
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument("description",
+                    type = str,
+                    help = "Bad request")
+parser.add_argument("submissions",
+                    type = dict,
+                    help = "Bad request",
+                    action = "append")
 
-encoder = pickle.load(open('./pre-trained-encoders/tf_vectorize', 'rb'))
-validate_model = pickle.load(open('./pre-trained-models/svm_tf', 'rb'))
-
+# Validation endpoint
 class ValidationEndPoint(Resource):
-    def get(self):
-        return {'hello': 'world'}
-    
+    validationService = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.validationService = ValidationService()
+
     def post(self):
+        # Get the parameters
         args = parser.parse_args()
         description = args.get("description")
+
+        return self.validationService.validate(description)
+
+# Clustering program endpoint
+class ClusteringProgram(Resource):
+    clusterService = None
+    def __init__(self) -> None:
+        super().__init__()
+        self.clusterService = ClusterService()
+
+    def post(self):
+        """
+            Input:
+            {
+                "submissions": [
+                    {1: "Submission 1"},
+                    {2: "Submission 2"},
+                    {3: "Submission 3"},
+                    {4: "Submission 4"}, ...
+                ]
+            }
+
+            Output:
+            {
+                "clusters": [
+                    ["1", "2", "3", ...],
+                    ["5", "6", "7", ...], ...
+                ],
+                "no_cluster": Integer
+            }
+        """
+
+        # Get the parameters
+        args = parser.parse_args()
+        submissions = args.get("submissions")
         
-        encode_description = encoder.transform({description: 1})
-        predict_value = validate_model.predict(encode_description)
+        return self.clusterService.clusterTestFunction(submissions)
 
-
-        return jsonify(
-            isValid = True if (predict_value == 0) else False
-        )
-
-api.add_resource(ValidationEndPoint, '/api/v1/validate-description')
 
 if __name__ == '__main__':
+    # Add endpoints
+    api.add_resource(ValidationEndPoint, 
+                     '/api/v1/validate-description',
+                     endpoint = "validate_endpoint")
+    api.add_resource(ClusteringProgram, 
+                     '/api/v1/cluster-program', 
+                     endpoint = "cluster_endpoint")
+    
+    # Run server
     app.run(debug=True)
